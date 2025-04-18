df6d5d7e47dd884b2d7ad9b62a9f218e9c977a04
Hemanth V <153289923+Hemanthmon@users.noreply.github.com>
2025-04-18 12:06:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-const team = [players]
-const team1 = [...players]
-const cap1 = [...person]
+// Reference
+const team = players;
 
+// Shallow copy of array using spread
+const team1 = [...players];
+
+// Shallow copy of object using spread
+const cap1 = { ...person };
+
+// Attaching to window for external access (as required)
 window.players = players;
 window.person = person;
 window.team = team;
